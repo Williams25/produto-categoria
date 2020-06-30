48ac5076639316e3863481ada82eb8b1b952cc3d
Williams25 <43673479+Williams25@users.noreply.github.com>
2020-06-30 12:24:57 -0300

    Foi ajustado o método de update

--- src/controllers/CategoriaController.js
+++ src/controllers/CategoriaController.js
@@ -1,69 +1,69 @@
-module.exports = function (app){
-    var controller = {};
-    var categoria = app.models.Categoria;
+module.exports = function (app) {
+  var controller = {};
+  var categoria = app.models.Categoria;
 
-    controller.salvaCategoria = function (req, res){
-        categoria.create(req.body).then(
-            function (categoria){
-                res.status(201).json(categoria)
-            }, function (erro){
-                console.error(erro);
-                res.status(500).json(erro);
-            }
-        );
-    };
+  controller.salvaCategoria = function (req, res) {
+    categoria.create(req.body).then(
+      function (categoria) {
+        res.status(201).json(categoria)
+      }, function (erro) {
+        console.error(erro);
+        res.status(500).json(erro);
+      }
+    );
+  };
 
-    controller.listaCategoria = function (req, res){
-        categoria.find().exec().then(
-            function (categoria){
-                res.status(200).json(categoria);
-            }, function (erro){
-                console.error(erro);
-                res.status(500).json(erro);
-            }
-        );
-    };
+  controller.listaCategoria = function (req, res) {
+    categoria.find().exec().then(
+      function (categoria) {
+        res.status(200).json(categoria);
+      }, function (erro) {
+        console.error(erro);
+        res.status(500).json(erro);
+      }
+    );
+  };
 
-    controller.alterarCategoria = function (req, res){
-        var _id = req.body.id;
-        categoria.findByIdAndUpdate(_id, req.body).exec().then(
-            function (categoria){
-                res.status(200).json(categoria);
-            }, function (erro){
-                console.error(erro);
-                res.status(500).json(erro);
-            }
-        );
-    };
+  controller.alterarCategoria = function (req, res) {
+    var _id = req.body.id;
+    categoria.findOneAndUpdate(_id, req.body).exec().then(
+      function (categoria) {
+        res.status(200).json(categoria);
+      }, function (erro) {
+        console.error(erro);
+        res.status(500).json(erro);
+      }
+    );
+  };
 
-    controller.removeCategoria = function (req,res){
-        var _id = req.params.id;
-        categoria.remove({"_id": _id}).exec().then(
-            function(categoria){
-                console.log(categoria)
-                res.status(204).end();
-            }, function(erro){
-                console.error(erro);
-                res.status(500).json(erro);
-            }
-        );
-    };
+  controller.removeCategoria = function (req, res) {
+    var _id = req.params.id;
+    categoria.remove({ "_id": _id }).exec().then(
+      function (categoria) {
+        console.log(categoria)
+        res.status(204).end();
+      }, function (erro) {
+        console.error(erro);
+        res.status(500).json(erro);
+      }
+    );
+  };
 
-    controller.obtemCategoria = function (req, res) {
-        var _id = req.params.id;
-        categoria.findById(_id).exec().then(
-            function(categoria){
-                if(!categoria){
-                    res.status(404).end();
-                }else{
-                    res.status(200).json(categoria);
-                }
-            }, function (erro){
-                console.error(erro);
-                res.status(500).json(erro);
-            }
-        );
-    }
+  controller.obtemCategoria = function (req, res) {
+    var _id = req.params.id;
+    categoria.findById(_id).exec().then(
+      function (categoria) {
+        if (!categoria) {
+          res.status(404).end();
+        } else {
+          res.status(200).json(categoria);
+        }
+      }, function (erro) {
+        console.error(erro);
+        res.status(500).json(erro);
+      }
+    );
+  }
 
-    return controller;
+  return controller;
 }
