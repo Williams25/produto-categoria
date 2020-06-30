65bd99b7cd4e70ae433d6f60a0057578a4a506c9
Williams25 <43673479+Williams25@users.noreply.github.com>
2020-06-30 16:37:34 -0300

    Foi adicionado validações no corpo da requisição e o json foi ajustado o retorno do json

--- src/controllers/EstoqueController.js
+++ src/controllers/EstoqueController.js
@@ -3,7 +3,24 @@ module.exports = function (app) {
   var estoque = app.models.Estoque;
 
   controller.salvaEstoque = function (req, res) {
-    estoque.create(req.body).then(
+    const { produto, quantidadedeMinimaEstoque, quantidadedeEstoque } = req.body;
+
+    if (!produto || !quantidadedeMinimaEstoque || !quantidadedeEstoque) return res.status(400).send({
+      mensagem: 'Campos invalidos!',
+      body: {
+        required: {
+          produto: 'ObjectId',
+          quantidadedeMinimaEstoque: 'Number',
+          quantidadedeEstoque: 'Number',
+        }
+      }
+    })
+
+    const body = {
+      produto, quantidadedeMinimaEstoque, quantidadedeEstoque
+    }
+
+    estoque.create(body).then(
       function (estoque) {
         res.status(201).json(estoque)
       }, function (erro) {
@@ -16,7 +33,21 @@ module.exports = function (app) {
   controller.listaEstoque = function (req, res) {
     estoque.find().populate('produto').exec().then(
       function (estoque) {
-        res.status(200).json(estoque);
+        const response = estoque.map(e => {
+          return {
+            _id: e._id,
+            quantidadedeMinimaEstoque: e.quantidadedeMinimaEstoque,
+            quantidadedeEstoque: e.quantidadedeEstoque,
+            produto: {
+              _id: e.produto._id,
+              categoria: e.produto.categoria,
+              descricao: e.produto.descricao,
+              valor: Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(e.produto.valor),
+              ativo: e.produto.ativo,
+            }
+          }
+        })
+        res.status(200).json({ quantidade: estoque.length, estoque: response });
       }, function (erro) {
         console.error(erro);
         res.status(500).json(erro);
@@ -25,8 +56,24 @@ module.exports = function (app) {
   };
 
   controller.alteraEstoque = function (req, res) {
-    var _id = req.body._id;
-    estoque.findByIdAndUpdate(_id, req.body).exec().then(
+    const { quantidadedeMinimaEstoque, quantidadedeEstoque, _id } = req.body;
+
+    if (!quantidadedeMinimaEstoque || !quantidadedeEstoque || !_id) return res.status(400).send({
+      mensagem: 'Campos invalidos!',
+      body: {
+        required: {
+          _id: 'ObjectId',
+          quantidadedeMinimaEstoque: 'Number',
+          quantidadedeEstoque: 'Number',
+        }
+      }
+    })
+
+    const body = {
+      quantidadedeMinimaEstoque, quantidadedeEstoque, _id
+    }
+
+    estoque.findByIdAndUpdate(_id, body).exec().then(
       function (estoque) {
         res.status(200).json(estoque);
       }, function (erro) {
@@ -37,8 +84,8 @@ module.exports = function (app) {
   }
 
   controller.removeEstoque = function (req, res) {
-    var _id = req.params.id;
-    estoque.remove({ "_id": _id }).exec().then(
+    const { id } = req.params;
+    estoque.remove({ "_id": id }).exec().then(
       function (estoque) {
         res.status(204).end();
       }, function (erro) {
@@ -49,13 +96,28 @@ module.exports = function (app) {
   }
 
   controller.obtemEstoque = function (req, res) {
-    var _id = req.params.id;
-    estoque.findById(_id).populate('produto').exec().then(
+    const { id } = req.params;
+    estoque.findById(id).populate('produto').exec().then(
       function (estoque) {
         if (!estoque) {
           res.status(404).end();
         } else {
-          res.status(200).json(estoque);
+          const response = {
+            _id: estoque._id,
+            quantidadedeMinimaEstoque: estoque.quantidadedeMinimaEstoque,
+            quantidadedeEstoque: estoque.quantidadedeEstoque,
+            created: estoque.created,
+            produto: {
+              _id: estoque.produto._id,
+              categoria: estoque.produto.categoria,
+              descricao: estoque.produto.descricao,
+              valor: Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(estoque.produto.valor),
+              ativo: estoque.produto.ativo,
+              created: estoque.produto.created,
+            }
+          }
+
+          res.status(200).json({ estoque: response });
         }
       }, function (erro) {
         console.error(erro);
